ae394df6896a624503a8599ae4bcf28a8416b70d
Joshua Cancellier <jcancellier@csub.edu>
2018-10-16 15:59:16 -0700

    added comments

--- App.js
+++ App.js
@@ -5,10 +5,10 @@ import { AppLoading } from 'expo';
 import { store, persistor } from 'app/src/redux/store';
 import Index from 'app/src';
 
-//redux store
-//1st argument is list of reducers
-//2nd argument is initial state
-//3rd argument is middleware (store enhancers)
+// redux store 
+// 1st argument is list of reducers
+// 2nd argument is initial state
+// 3rd argument is middleware (store enhancers)
 
 export default class App extends React.Component {
 
