9d984873e4507714dcc8a20bdf1e92fca0ca5252
jfrench661 <jakefrench1984@gmail.com>
2018-10-16 21:59:16 -0700

    created business bio screen

--- src/screens/business/BusinessBioScreen.js
+++ src/screens/business/BusinessBioScreen.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { SafeAreaView, ScrollView, Text } from 'react-native';
+import { SafeAreaView, ScrollView, View, StyleSheet, ImageBackground } from 'react-native';
 import { store } from 'app/src/redux/store';
-import { Card } from 'react-native-elements';
+import { Divider, Avatar, Text, ButtonGroup } from 'react-native-elements';
 import api from 'app/src/api';
+import { Ionicons } from '@expo/vector-icons';
 
 class BusinessBioScreen extends React.Component {
 	constructor(props) {
@@ -10,35 +11,167 @@ class BusinessBioScreen extends React.Component {
 		this.state = {
 			name: '',
 			about: '',
-			street: '',
-			city: '',
-			state: '',
-			zip: '',
-			primaryPhone: '',
-			altPhone: '',
-			email: ''
+			street: '4900 University Way',
+			city: 'Bakersfield',
+			state: 'CA',
+			zip: '93301',
+			primaryPhone: '(661) 555-5213',
+			altPhone: '(661) 555-1234',
+			email: 'SomeCompany@gmail.com',
 		}
 	}
+
+
+	componentWillMount(){
+
+	}
+
 	render() {
+		const buttons = [<Ionicons name='md-heart' size={28} color={theme.onBackground} />,
+		<Ionicons name='md-paper-plane' size={28} color={theme.onBackground} />,
+		<Ionicons name='md-create' size={28} color={theme.onBackground} />
+		];
+		const { selectedIndex } = this.state;
 		return (
-			<SafeAreaView>
-				<ScrollView>
-					<Card title='About Us'>
-						<Text>
-							{this.state.about}
-						</Text>
-					</Card>
-					<Card title='Contact Information'>
-						<Text>
-							{this.state.street}{'\n'}
-							{this.state.city} {this.state.state} {this.state.zip}{'\n'}
-							{this.state.primaryPhone}
-						</Text>
-					</Card>
+			<SafeAreaView stysty={styles.container}>
+				<ImageBackground
+					style={styles.header}
+					source={{ uri: ('http://www.camranhbaync.com/wp-content/uploads/2014/01/main-header.jpg') }}
+				>
+					<View style={styles.avatarView}>
+						<Avatar
+							style={styles.avatar}
+							medium
+							source={{
+								uri:
+									"https://cdn2.unrealengine.com/Epic+Games+Node%2Fxlarge_whitetext_blackback_epiclogo_504x512_1529964470588-503x512-ac795e81c54b27aaa2e196456dd307bfe4ca3ca4.jpg"
+							}}
+							onPress={() => console.log("Works!")}
+							activeOpacity={0.7}
+						/>
+						<View styles={styles.headerTextView}>
+							<Text h3 style={styles.nameText}>Business Name</Text>
+							<Text h4 style={styles.subText}>Subtext</Text>
+						</View>
+
+					</View>
+				</ImageBackground>
+
+				<ButtonGroup
+					onPress={() => console.log("pressed")}
+					selectedIndex={selectedIndex}
+					buttons={buttons}
+					containerStyle={{ height: 50 }}
+				/>
+
+
+
+				<ScrollView style={styles.scrollView}>
+					<View style={styles.infoCard}>
+						<Text style={{
+							fontSize: 20, fontWeight: 'bold', color: theme.onBackground,
+						}}>Contact Info</Text>
+
+						<Divider style={{ backgroundColor: theme.onBackground, marginBottom: 10 }} />
+						<Text style={styles.aboutText}>Phone: {this.state.primaryPhone}</Text>
+						<Text style={styles.aboutText}>Alt Phone: {this.state.altPhone}</Text>
+						<Text style={styles.aboutText}>Email: {this.state.email}</Text>
+						<Text style={styles.aboutText}>{this.state.street}</Text>
+						<Text style={styles.aboutText}>{this.state.city}, {this.state.state} {this.state.zip}</Text>						
+					</View>
+					<View style={styles.infoCard}>
+						<Text style={{
+							fontSize: 20, fontWeight: 'bold', color: theme.onBackground,
+						}}>About</Text>
+
+						<Divider style={{ backgroundColor: theme.onBackground, marginBottom: 10 }} />
+						<Text style={styles.aboutText}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy
+						text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only
+						five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset
+						sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy
+						text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only
+						five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset
+						sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy
+						text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only
+						five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset
+						sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy
+						text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only
+						five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset
+						sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy
+						text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only
+						five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset
+						sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy
+						text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only
+						five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset
+						sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy
+						text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only
+						five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset
+						sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</Text>
+					</View>
+
 				</ScrollView>
 			</SafeAreaView>
 		);
 	}
 }
+
+
+const theme = store.getState().settings.theme;
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	scrollView: {
+		width: '100%',
+		marginBottom: 100
+	},
+	avatar: {
+		width: 80,
+		height: 80,
+		marginRight: 10
+	},
+	avatarView: {
+		margin: 10,
+		flexDirection: 'row'
+	},
+	header: {
+		width: '100%',
+		height: 200,
+		flexDirection: 'row',
+		alignItems: 'flex-end'
+	},
+	nameText: {
+		color: theme.onPrimary,
+		textShadowColor: 'rgba(0, 0, 0, 1)',
+		textShadowOffset: { width: -1, height: 1 },
+		textShadowRadius: 10,
+		marginBottom: 5,
+		fontSize: 28
+	},
+	subText: {
+		color: theme.onPrimary,
+		textShadowColor: 'rgba(0, 0, 0, 1)',
+		textShadowOffset: { width: -1, height: 1 },
+		textShadowRadius: 10,
+		fontSize: 20
+	},
+	headerTextView: {
+		flexDirection: 'row',
+		marginLeft: 10
+	},
+	infoCard: {
+		flex: 1,
+		backgroundColor: theme.background,
+		padding: 10,
+		margin: 10,
+		elevation: 5,
+		borderRadius: 10
+	},
+	aboutText: {
+		color: theme.onBackground
+	}
+});
 
 export default BusinessBioScreen;
