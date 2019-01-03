84671edd0c24f9b200dbfddbc72da12394dc677f
jfrench661 <jakefrench1984@gmail.com>
2019-01-03 02:52:45 -0800

    updated component

--- src/components/common/CustomPicker.js
+++ src/components/common/CustomPicker.js
@@ -33,9 +33,11 @@ const IndustryPicker = (props) => {
 	return (
 		<Surface style={{ height: 50, width: '90%', elevation: 5, borderRadius: 5, margin: 10 }}>
 			<Picker {...props}>
+				{/* All picker values must be lower case */}
 				<Picker.Item label="Select Industry..." value="" />
-				<Picker.Item label="Cannabis" value="Cannabis" />
-				<Picker.Item label="Technology" value="Technology" />
+				<Picker.Item label="Cannabis" value="cannabis" />
+				<Picker.Item label="I.T." value="computer" />
+				<Picker.Item label="Pest Control" value="pestcontrol" />
 			</Picker>
 		</Surface>
 	);
